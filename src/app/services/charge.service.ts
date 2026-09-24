09b63c00ba79780bb16c72f917d3d69946058df7
hurbbie <nattaphon.l@iprogressx.co.th>
2026-09-24 12:40:52 +0700

    feat(connect api)

--- src/app/services/charge.service.ts
+++ src/app/services/charge.service.ts
@@ -1,6 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -8,7 +10,7 @@ export class ChargeService {
   private readonly http = inject(HttpClient);
 
   charge(idToken: string, amount: number) {
-    return this.http.post('http://localhost:8080/api/charge', {
+    return this.http.post(`${environment.apiUrl}/api/charge`, {
       idToken,
       amount,
     });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
 
   liffId: '2011615014-RbQ12zZy',
+  apiUrl: 'https://herbie-charge-api.onrender.com',
 };
